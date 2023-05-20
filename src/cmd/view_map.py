f2d437b7e627cf4ad9743b157976449bf617af8c
Adel Suarez <adel90suarez@gmail.com>
2023-05-19 22:17:43 -0400

    se corrigieron varios errores en la impresion del mapa

--- src/cmd/view_map.py
+++ src/cmd/view_map.py
@@ -1,8 +1,6 @@
 import style.style as st
 
 class View_map:
-    count = 1
-    position = 1
     
     def __init__(self,row, map_game) -> None:
         self._row = row
@@ -10,52 +8,66 @@ class View_map:
 
 
     def view(self):
+        position = 1
 
         self._number_up()
         
         for i in range(len(self.map_game)):
             
+            view_number = True
+            count = 1
             # Print que crea la linea superior del mapa 
             self._separator_line()
-
-            view_number = True
+            
 
             # Ciclos que imprime el mapa sin los [] de las listas
             for j in self.map_game[i]:
 
                 # TODO: optimizar el codigo
-                self._number_left(view_number)
+                if view_number:
+                    if position >= 10:
+                        print(f'{st.BLUE}{position}{st.RESET}', end='')
+                    else:
+                        print(f'{st.BLUE}{position}{st.RESET} ', end='')
+                    position+=1
 
-                if View_map.count < self._row:
+                if count < self._row:
                     if j ==  '>' or j ==  '<' or j ==  '^' or j ==  'v': 
-                        self._view_assest(j, st.GREEN)
+                        print(f'| {st.GREEN}{j}{st.RESET} ', end='')
                         view_number=False
+                        count+=1
 
                     elif j == '*' or j=='#':
-                        self._view_assest(j, st.RED)
+                        print(f'| {st.RED}{j}{st.RESET} ', end='')
                         view_number=False
+                        count+=1
 
                     elif j == 'H':
-                        self._view_assest(j, st.YELLOW)
+                        print(f'| {st.YELLOW}{j}{st.RESET} ', end='')
                         view_number=False
+                        count+=1
 
                     else:
                         print(f'| {j} ', end='')
                         view_number=False
-                        View_map.count+=1
-                elif View_map.count == self._row:
+                        count+=1
+                elif count == self._row:
                     if j ==  '>' or j ==  '<' or j ==  '^' or j ==  'v':
-                        self._view_assest_r(j, st.GREEN)
+                        # self._view_assest_r(j, st.GREEN, count)
+                        print(f'| {st.GREEN}{j}{st.RESET} |', end='')
+                        count+=1
 
                     elif j == '*' or j=='#':
-                        self._view_assest_r(j, st.RED)
+                        print(f'| {st.RED}{j}{st.RESET} |', end='')
+                        count+=1
                         
                     elif j == 'H':
-                        self._view_assest_r(j, st.YELLOW)
+                        print(f'| {st.YELLOW}{j}{st.RESET} |', end='')
+                        count+=1
 
                     else:
                         print(f'| {j} |', end='')
-                        View_map.count = 1
+                        count = 1
 
             print('')
         # Print que crea la linea inferior del mapa 
@@ -71,22 +83,6 @@ class View_map:
             else:
                 print(f'  {st.BLUE}{i+1}{st.RESET} ', end='')
         print('')
-
-    def _number_left(self, view_number):
-        if view_number:
-            if View_map.position >= 10:
-                print(f'{st.BLUE}{View_map.position}{st.RESET}', end='')
-            else:
-                print(f'{st.BLUE}{View_map.position}{st.RESET} ', end='')
-            View_map.position+=1
-
-    def _view_assest(self, j, color):
-        print(f'| {color}{j}{st.RESET} ', end='')
-        View_map.count+=1
-
-    def _view_assest_r(self, j, color):
-        print(f'| {color}{j}{st.RESET} |', end='')
-        View_map.count = 1
 
     def _separator_line(self):
         print('  ', end='')
