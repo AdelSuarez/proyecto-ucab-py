25d7f7f3a133e5dc3a18fe6986f5e4be9254ff1a
Adel Suarez <adel90suarez@gmail.com>
2023-06-20 20:00:07 -0400

    Eliminacion de la parte de interfaz

--- Manager.py
+++ Manager.py
@@ -1,6 +1,4 @@
 from cmd.main_menu import Main_menu
-from screen.main_screen import Main_screen
-# from src.screen.main_screen import Main_screen
 class Manager:
 
     def __init__(self):
@@ -8,19 +6,4 @@ class Manager:
     
 
     def views(self):
-        # print('BIENVENIDO')
-        # print('1 | Jugar la version de terminal')
-        # print('2 | Jugar la version de grafica')
-
-        # option = int(input('Introduce la opcion >> '))
-        # if option == 1:
-        # hola mufno
-        
         Main_menu()
-        # elif option == 2:
-        # Main_screen()
-            # print('grafica')
-            #ksjdhgf
-
-        # scoot deja de ser tan awueboniado por di
-            # print('grafica')
