d22c959985122217d75f2946b020d0aa7105f61d
adel suarez <adel90suarez@gmail.com>
2023-06-23 11:02:44 -0400

    implementacion de un archivo que contiene todas las imagenes para mejor importacion, y nuevas imagenes

---
+++ assets/Assets.py
@@ -0,0 +1,12 @@
+import pygame
+
+robot = pygame.transform.scale(pygame.image.load("assets/robot1.png"), (50, 50))
+bomb = pygame.transform.scale(pygame.image.load("assets/bomba.png"), (35, 35))
+goal = pygame.transform.scale(pygame.image.load("assets/unavainaloca.png"), (35, 35))
+over = pygame.transform.scale(pygame.image.load("assets/explosion.png"), (45, 45))
+victory = pygame.transform.scale(pygame.image.load("assets/victory.png"), (50, 50))
+btn_start = pygame.image.load("assets/start_btn.png")
+btn_exit = pygame.image.load("assets/exit_btn.png")
+game_over_img =  pygame.transform.scale(pygame.image.load("assets/Background1.png"), (600,400)) 
+victory_img =  pygame.transform.scale(pygame.image.load("assets/victory.png"), (700,500))
+b = pygame.image.load("assets/b_opacity.png")
