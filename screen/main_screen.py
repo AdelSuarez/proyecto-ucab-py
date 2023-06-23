5e0268f42669fe94efb2c754482d920417919cbe
adel suarez <adel90suarez@gmail.com>
2023-06-23 11:04:24 -0400

    Implemetacion de la nuevas importaciones, y cambios en el codigo para su uso

--- screen/main_screen.py
+++ screen/main_screen.py
@@ -1,5 +1,5 @@
 import pygame
-# from src.create_map import Create_map
+
 import style.style as st
 from src.create_map import Create_map
 from src.move import Move
@@ -7,6 +7,8 @@ from src.robot_rotation import Robot_rotation
 from src.collision_checker import Collision_checker
 from screen.create_map_screen import Create_map_screen
 from components.Button import Button
+from components.Text import Text
+import assets.Assets as ast
 
 class Main_screen:
     def __init__(self):
@@ -19,15 +21,16 @@ class Main_screen:
 
         # ------------------------ ASSETS -----------------------------------------------------
         # self._robot= pygame.transform.scale(pygame.image.load("screen/assets/robot.png"), (40, 40))
-        self._robot = pygame.transform.scale(pygame.image.load("assets/robot1.png"), (50, 50))
-        self._bomb = pygame.transform.scale(pygame.image.load("assets/bomba.png"), (35, 35))
-        self._goal = pygame.transform.scale(pygame.image.load("assets/unavainaloca.png"), (35, 35))
-        self._over = pygame.transform.scale(pygame.image.load("assets/explosion.png"), (45, 45))
-        self._victory = pygame.transform.scale(pygame.image.load("assets/victory.png"), (50, 50))
-        self._btn_start = pygame.image.load("assets/start_btn.png")
-        self._btn_exit = pygame.image.load("assets/exit_btn.png")
-        self._game_over_img =  pygame.transform.scale(pygame.image.load("assets/Background1.png"), (600,400)) 
-        self._victory_img =  pygame.transform.scale(pygame.image.load("assets/victory.png"), (700,500)) 
+        # self._robot = pygame.transform.scale(pygame.image.load("assets/robot1.png"), (50, 50))
+        # self._bomb = pygame.transform.scale(pygame.image.load("assets/bomba.png"), (35, 35))
+        # self._goal = pygame.transform.scale(pygame.image.load("assets/unavainaloca.png"), (35, 35))
+        # self._over = pygame.transform.scale(pygame.image.load("assets/explosion.png"), (45, 45))
+        # self._victory = pygame.transform.scale(pygame.image.load("assets/victory.png"), (50, 50))
+        # self._btn_start = pygame.image.load("assets/start_btn.png")
+        # self._btn_exit = pygame.image.load("assets/exit_btn.png")
+        # self._game_over_img =  pygame.transform.scale(pygame.image.load("assets/Background1.png"), (600,400)) 
+        # self._victory_img =  pygame.transform.scale(pygame.image.load("assets/victory.png"), (700,500))
+        # self.b = pygame.image.load("assets/b_opacity.png")
 
 
 
@@ -42,11 +45,6 @@ class Main_screen:
         pygame.display.set_caption('ROBOTcok')
         self._clock = pygame.time.Clock()
         
-
-    def draw_text(self,text, font, text_col, x, y):
-        img = font.render(text, True, text_col)
-        self._screen.blit(img, (x,y))
-
 
     def _game(self):
          while not self._game_over:
@@ -69,7 +67,7 @@ class Main_screen:
                         self._menu()
 
             self._screen.fill(st.BLACK)
-            Create_map_screen(self.map_game, self._screen, self._robot, self.address, self._bomb, self._goal, self._victory, self._over)
+            Create_map_screen(self.map_game, self._screen, ast.robot, self.address, ast.bomb, ast.goal, ast.victory, ast.over)
 
             if Collision_checker(self.map_game).checker() == '#':
                 self._screen_game_over()
@@ -88,10 +86,10 @@ class Main_screen:
                 self._game()
             else:
                 # Menu
-                self.draw_text('MENU PRINCIPAL', self._font, st.WHITE, 200,50)
-                if Button(300,300,self._btn_start,1.5).draw(self._screen):
+                Text('MENU PRINCIPAL', self._font, st.WHITE,).draw_text(self._screen, 200,50)
+                if Button(300, 300, ast.btn_start, 1.5).draw(self._screen):
                     self._game_pause = True
-                elif Button(390,600,self._btn_exit,1).draw(self._screen):
+                elif Button(390,600,ast.btn_exit,1).draw(self._screen):
                     pygame.quit()
                     self._game_over = True
 
@@ -104,18 +102,14 @@ class Main_screen:
                     if event.key == pygame.K_SPACE:
                         self._game_pause = True
                     
-                        
-                        
-                
-
-
             pygame.display.update()
             self._clock.tick(60)
 
     def _screen_game_over(self):
         while not self._game_over:
-            self._screen.fill((52,78,91))
-            self._screen.blit(self._game_over_img, (220,180))
+            self._screen.blit(ast.b, (0,0))
+
+            self._screen.blit(ast.game_over_img, (220,180))
 
 
             
@@ -132,11 +126,9 @@ class Main_screen:
 
     def _screen_victory(self):
         while not self._game_over:
-            self._screen.fill((60,78,91))
-            self._screen.blit(self._victory_img, (150,120))
+            self._screen.blit(ast.b, (0,0))
+            self._screen.blit(ast.victory_img, (150,120))
 
-
-            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
