fe39e359bab5d6e3b1b33176400f9baca4b379e3
adel suarez <adel90suarez@gmail.com>
2023-06-24 23:59:47 -0400

    Se implemento un text de la version y correcciones de codigo

--- screen/Screen_menu.py
+++ screen/Screen_menu.py
@@ -7,14 +7,14 @@ from screen.Screen_game import Screen_game
 
 
 class Screen_menu:
-    def __init__(self, game_over, game_pause, address, map_game, clock) -> None:
-        self._font = pygame.font.SysFont("arialblack", 70)
+    def __init__(self, game_over, game_pause, address, map_game) -> None:
+        self._font = pygame.font.Font("assets/font.ttf", 100)
+        self._font_v = pygame.font.Font("assets/font.ttf", 20)
+
         self.game_over = game_over
         self.game_pause = game_pause
         self.address = address
         self.map_game = map_game
-        self.clock = clock
-
 
     def menu(self, screen):
         while not self.game_over:
@@ -22,14 +22,16 @@ class Screen_menu:
 
             if self.game_pause:
                 #game
-                Screen_game(self.game_over, self.address, self.map_game, self.game_pause, self.clock).game(screen)
+                Screen_game(self.game_over, self.address, self.map_game, self.game_pause).game(screen)
             else:
                 # Menu
-                Text('MENU PRINCIPAL', self._font, st.WHITE,).draw_text(screen, 200,50)
+                Text('ROBOTcok', self._font, st.WHITE).draw_text(screen, 130,80)
+                Text('v2.4.5.1', self._font_v, st.GREEN_ROBOT).draw_text(screen, 870,780)
                 if Button(300, 300, asset.btn_start, 1.5).draw(screen):
                     self.game_pause = True
-                elif Button(390,600,asset.btn_exit,1).draw(screen):
+                elif Button(390,650,asset.btn_exit,1).draw(screen):
                     pygame.quit()
+
                     self.game_over = True
 
             
@@ -42,4 +44,3 @@ class Screen_menu:
                         self.game_pause = True
                     
             pygame.display.update()
-            self.clock.tick(60)
