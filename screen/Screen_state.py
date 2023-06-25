fa003f90ef6c14277a40cee8e91bcb7c138fba75
adel suarez <adel90suarez@gmail.com>
2023-06-25 00:00:28 -0400

    Se elimino codigo basura

--- screen/Screen_state.py
+++ screen/Screen_state.py
@@ -1,10 +1,12 @@
 import pygame
+from components.Text import Text
+import style.style as st
 class Screen_state:
-    def __init__(self, backgorund, asset, game_over, clock) -> None:
+    def __init__(self, backgorund, game_over, asset = None) -> None:
+        self._font = pygame.font.Font("assets/font.ttf", 100)
         self.background = backgorund
         self._asset = asset
         self._game_over = game_over
-        self.clock = clock
 
 
     def victory(self, screen):
@@ -16,23 +18,18 @@ class Screen_state:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     self._game_over = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self._game_pause = True
+
             pygame.display.update()
-            self.clock.tick(60)
 
     def game_over(self, screen):
         while not self._game_over:
             screen.blit(self.background, (0,0))
-            screen.blit(self._asset, (220,180))
+
+            Text('Game Over', self._font, st.WHITE,).draw_text(screen, 80,300)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     self._game_over = True
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self._game_pause = True
+
             pygame.display.update()
-            self.clock.tick(60)
