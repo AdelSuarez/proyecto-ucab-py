be7b6e42730bff8989bc48ff014e7daecb0938cf
adel suarez <adel90suarez@gmail.com>
2023-06-24 23:56:45 -0400

    Se elimino importaciones de assets en la importacion y se introdujo la fuente

--- assets/Assets.py
+++ assets/Assets.py
@@ -7,6 +7,5 @@ over = pygame.transform.scale(pygame.image.load("assets/explosion.png"), (45, 45
 victory = pygame.transform.scale(pygame.image.load("assets/victory.png"), (50, 50))
 btn_start = pygame.image.load("assets/start_btn.png")
 btn_exit = pygame.image.load("assets/exit_btn.png")
-game_over_img =  pygame.transform.scale(pygame.image.load("assets/Background1.png"), (600,400)) 
 victory_img =  pygame.transform.scale(pygame.image.load("assets/victory.png"), (700,500))
-b = pygame.image.load("assets/b_opacity.png")
+BG_opacity = pygame.image.load("assets/b_opacity.png")
